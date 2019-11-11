aada4d6b0b5ada0d9f8b95405dd7242f6eca56f2
yutveg <kodi.0n31l@gmail.com>
2019-11-11 14:43:45 -0800

    functionality complete. movie items render on click

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import MovieList from './MovieList.js';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  console.log(props.match.params.id);
   useEffect(() => {
-    const id = props.id;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
-          setMovie(response.data);
+          console.log(response);
+          setMovie(response.data); 
         })
         .catch(error => {
           console.error(error);
         });
 
-  },[props.id]);
+  },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -30,8 +30,7 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-  const movieItem = movie.find(item => item.id === Number(props.match.params.dataID))
-  const { title, director, metascore, stars } = movieItem;
+  const { title, director, metascore, stars } = movie;
   
   return (
     <div className="save-wrapper">
